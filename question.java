b50810b4bb0dfbaf6642124cff92226b34c25f22
Harshita Gurjar <harshi06-ops>
2025-08-28 09:14:40 +0530

    initial commit

---
+++ question.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main {
+public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+int n=input.nextInt();
+int sum = 0;
+sum = (n*(n+1))/2;
+System.out.println(sum);
+}
+}
